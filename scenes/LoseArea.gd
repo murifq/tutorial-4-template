9a71be40627b3c47d0e222018612b9a5b6a37560
murifq <muhamad.rifqi510@gmail.com>
2025-03-07 13:31:02 +0700

    fix snake case naming variable

--- scenes/LoseArea.gd
+++ scenes/LoseArea.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
-@export var sceneName: String = "LoseScreen"
+@export var scene_name: String = "LoseScreen"
 
 
 func _on_body_entered(body):
 	if body.get_name() == "Player":
-		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+		get_tree().change_scene_to_file(str("res://scenes/" + scene_name + ".tscn"))
